34d9a184c653900baa554393807789c571a1b01b
corofides <jamlendrem@gmail.com>
2026-01-07 07:16:20 +0000

    Move most of run into repl, add error handling into run.

--- src/editor.rs
+++ src/editor.rs
@@ -11,48 +11,30 @@ impl Editor {
         Editor{} // default constructor, returns an editor. I guess we can skip this if we want.
     }
     pub fn run(&self) {
-        let _ = enable_raw_mode();
+        if let Err(err) = self.repl() {
+            panic!("{err:#?}");
+        }
+        print!("Goodbye.\r\n");
+    }
+    pub fn repl(&self) -> Result<(), std::io::Error> {
+        enable_raw_mode()?;
 
         // use crossterm for input instead of stdin,
         loop {
-            match read() {
-                Ok(Key(event)) => {
-                    println!("{event:?} \r");
-
-                    if let Char(c) = event.code {
-                        if c == 'q' {
-                            break;
-                        }
+            if let Key(event) = read()? {
+                println!("{event:?} \r");
+                
+                if let Char(c) = event.code {
+                    if c == 'q' {
+                        break;
                     }
-                },
-                Err(err) => {
-                    println!("Error: {err}");
-                },
-                _ => ()
+                }
+                
             }
         }
 
-
-        // for b in io::stdin().bytes() {
-        //     match b {
-        //       Ok(b) => {
-        //             let c = b as char;
-        //             if c.is_control() {
-        //                 println!("Binary: {0:08b} Ascii: {0:#03} \r", b);
-        //             } else {
-        //                 println!("Binary: {0:08b} Ascii: {0:#03} Character: {1:#?} \r", b, c);
-        //             }
-        //             if c == 'q' {
-        //                 break;
-        //             }
-        //         },
-        //         Err(err) => {
-        //             println!("Error: {}", err);
-        //         }
-        //     }
-
-        // }
-        let _ = disable_raw_mode();
+        disable_raw_mode()?;
+        return Ok(());
 
     }
 }
