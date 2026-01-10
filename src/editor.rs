4094a4755d7208ea666ad4c650e3afb52175894e
corofides <jamlendrem@gmail.com>
2026-01-10 06:37:31 +0000

    Add welcome message and version number to middle of screen. Assignment 3

--- src/editor.rs
+++ src/editor.rs
@@ -45,7 +45,31 @@ impl Editor {
         Terminal::flush()?;
         Ok(())
     }
-    
+    fn draw_welcome() -> Result<(), Error> {
+
+        let Size { height, width } = Terminal::size()?;
+        let welcome_message = String::from("Welcome to Hecto");
+        let version = String::from("Version: 0.0.0");
+
+        #[allow(clippy::cast_possible_truncation)]
+        let welcome_length = welcome_message.len() as u16;
+        #[allow(clippy::cast_possible_truncation)]
+        let version_length = version.len() as u16;
+
+        let half_width = width / 2;
+        let half_height = height / 2;
+
+        let rows = 3;
+
+        Terminal::move_cursor_to(Position {x: half_width - (welcome_length / 2), y: half_height - (rows / 2)})?;
+        Terminal::print(&welcome_message[..])?; // print slice of welcome message.
+        
+        Terminal::move_cursor_to(Position {x: half_width - (version_length / 2), y: half_height + (rows / 2)})?;
+        Terminal::print(&version[..])?; // print slice of version number.
+
+        Terminal::flush()?;
+        Ok(())
+    }
     pub fn evaluate_event(&mut self, event: &Event) {
         if let Key(KeyEvent {
             code, modifiers, .. 
@@ -65,6 +89,7 @@ impl Editor {
             Terminal::print("Goodbye.\r\n")?;
         } else {
             Self::draw_rows()?;
+            Self::draw_welcome()?;
             Terminal::move_cursor_to(Position {x: 0, y: 0})?;
         }
         Terminal::show_cursor()?;
