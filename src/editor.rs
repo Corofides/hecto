aa87e0d6902cd252909fc588fc26c6518b76f2d3
corofides <jamlendrem@gmail.com>
2026-01-11 19:59:34 +0000

    Move login for printing rows to new view struct

--- src/editor.rs
+++ src/editor.rs
@@ -6,11 +6,13 @@ use std::io::Error;
 use std::cmp::{ min };
 
 mod terminal;
+mod view;
 
 use terminal::{Terminal, Size, Position};
+use view::{View};
 
-const NAME: &str = env!("CARGO_PKG_NAME");
-const VERSION: &str = env!("CARGO_PKG_VERSION");
+// const NAME: &str = env!("CARGO_PKG_NAME");
+// const VERSION: &str = env!("CARGO_PKG_VERSION");
 
 pub struct Editor {
     should_quit: bool,
@@ -101,49 +103,49 @@ impl Editor {
         Ok(())
 
     }
-    fn draw_empty_row() -> Result<(), Error> {
-        Terminal::print("~")?;
-        Ok(())
-    }
-    fn draw_rows() -> Result<(), Error> {
-        let Size { height, .. } = Terminal::size()?;
-        Terminal::move_caret_to(Position {x: 0, y: 0})?;
-        
-        for current_row in 0..height {
-            Terminal::clear_line()?;
+    // fn draw_empty_row() -> Result<(), Error> {
+    //     Terminal::print("~")?;
+    //     Ok(())
+    // }
+    // fn draw_rows() -> Result<(), Error> {
+    //     let Size { height, .. } = Terminal::size()?;
+    //     Terminal::move_caret_to(Position {x: 0, y: 0})?;
+    //     
+    //     for current_row in 0..height {
+    //         Terminal::clear_line()?;
 
-            #[allow(clippy::integer_division)]
-            if current_row == height / 3 {
-                Self::draw_welcome_message()?;
-            } else {
-                Self::draw_empty_row()?;
-            }
-            
-            if current_row.saturating_add(1) < height {
-                Terminal::print("\r\n")?; //print!("\r\n");
-            }
-        }
-        Terminal::flush()?;
-        Ok(())
-    }
-    fn draw_welcome_message() -> Result<(), Error> {
+    //         #[allow(clippy::integer_division)]
+    //         if current_row == height / 3 {
+    //             Self::draw_welcome_message()?;
+    //         } else {
+    //             Self::draw_empty_row()?;
+    //         }
+    //         
+    //         if current_row.saturating_add(1) < height {
+    //             Terminal::print("\r\n")?; //print!("\r\n");
+    //         }
+    //     }
+    //     Terminal::flush()?;
+    //     Ok(())
+    // }
+    // fn draw_welcome_message() -> Result<(), Error> {
 
-        let mut welcome_message = format!("{NAME} editor -- version {VERSION}");
+    //     let mut welcome_message = format!("{NAME} editor -- version {VERSION}");
 
-        let width = Terminal::size()?.width;
-        let len = welcome_message.len();
+    //     let width = Terminal::size()?.width;
+    //     let len = welcome_message.len();
 
-        #[allow(clippy::integer_division)]
-        let padding = (width.saturating_sub(len)) / 2;
+    //     #[allow(clippy::integer_division)]
+    //     let padding = (width.saturating_sub(len)) / 2;
 
-        let spaces = " ".repeat(padding.saturating_sub(1));
+    //     let spaces = " ".repeat(padding.saturating_sub(1));
 
-        welcome_message = format!("~{spaces}{welcome_message}");
-        welcome_message.truncate(width);
+    //     welcome_message = format!("~{spaces}{welcome_message}");
+    //     welcome_message.truncate(width);
 
-        Terminal::print(welcome_message)?;
-        Ok(())
-    }
+    //     Terminal::print(welcome_message)?;
+    //     Ok(())
+    // }
     pub fn evaluate_event(&mut self, event: &Event) -> Result<(), Error> {
         if let Key(KeyEvent {
             code, modifiers, .. 
@@ -175,7 +177,8 @@ impl Editor {
             Terminal::clear_screen()?;
             Terminal::print("Goodbye.\r\n")?;
         } else {
-            Self::draw_rows()?;
+            //Self::draw_rows()?;
+            View::render()?;
             Terminal::move_caret_to(Position {
                 x: self.position.x, 
                 y: self.position.y
---
+++ src/editor/view.rs
@@ -0,0 +1,57 @@
+use std::io::Error;
+use super::terminal::{Terminal, Size, Position};
+
+
+pub struct View {}
+
+const NAME: &str = env!("CARGO_PKG_NAME");
+const VERSION: &str = env!("CARGO_PKG_VERSION");
+
+impl View {
+    fn draw_empty_row() -> Result<(), Error> {
+        Terminal::print("~")?;
+        Ok(())
+    }
+    pub fn draw_welcome_message() -> Result<(), Error> {
+        let mut welcome_message = format!("{NAME} editor -- version {VERSION}");
+
+        let width = Terminal::size()?.width;
+        let len = welcome_message.len();
+
+        #[allow(clippy::integer_division)]
+        let padding = (width.saturating_sub(len)) / 2;
+
+        let spaces = " ".repeat(padding.saturating_sub(1));
+
+        welcome_message = format!("~{spaces}{welcome_message}");
+        welcome_message.truncate(width);
+
+        Terminal::print(welcome_message)?;
+        Ok(())
+    }
+    pub fn render() -> Result<(), Error> {
+        let Size { height, .. } = Terminal::size()?;
+        Terminal::move_caret_to(Position {x: 0, y: 0})?;
+        
+        for current_row in 0..height {
+            Terminal::clear_line()?;
+
+            #[allow(clippy::integer_division)]
+            if current_row == height / 3 {
+                Self::draw_welcome_message()?;
+            } else {
+                Self::draw_empty_row()?;
+            }
+
+            if current_row == 0 {
+                Terminal::print("Hello, world!")?;
+            }
+            
+            if current_row.saturating_add(1) < height {
+                Terminal::print("\r\n")?; //print!("\r\n")?;
+            }
+        }
+        Terminal::flush()?;
+        Ok(())
+    }
+}
