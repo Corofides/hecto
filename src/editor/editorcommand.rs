fd32a354779c13601bcd5733a0acf4cd5e06915f
corofides <jamlendrem@gmail.com>
2026-01-29 17:46:50 +0000

    Fix issue with restructured code

--- src/editor/editorcommand.rs
+++ src/editor/editorcommand.rs
@@ -79,7 +79,7 @@ impl TryFrom<Event> for ControlCommand {
 
 #[derive(Clone, Copy)]
 pub enum DisplayCommand {
-    Resize(size),
+    Resize(Size),
 }
 
 impl TryFrom<Event> for DisplayCommand {
@@ -91,7 +91,7 @@ impl TryFrom<Event> for DisplayCommand {
                 height: height_u16 as usize,
                 width: width_u16 as usize,
             })),
-            _ => Err(format!("Key Code not supported: {code:?}")),
+            _ => Err(format!("Event not supported: {event:?}")),
         }
     }
 }
@@ -108,39 +108,39 @@ pub enum EditorCommand {
     Save,
 }*/
 
-#[allow(clippy::as_conversions)]
-impl TryFrom<Event> for EditorCommand {
-    type Error = String;
-
-    fn try_from(event: Event) -> Result<Self, Self::Error> {
-        match event {
-            Event::Key(KeyEvent {
-                code, modifiers, ..
-            }) => match (code, modifiers) {
-                (KeyCode::Char('q'), KeyModifiers::CONTROL) => Ok(Self::Quit),
-                (KeyCode::Char('s'), KeyModifiers::CONTROL) => Ok(Self::Save),
-                (KeyCode::Char(character), KeyModifiers::NONE | KeyModifiers::SHIFT) => {
-                    Ok(Self::Insert(character))
-                },
-                (KeyCode::Tab, _) => Ok(Self::Insert('\t')),
-                (KeyCode::Enter, _) => Ok(Self::Enter),
-                (KeyCode::Up, _) => Ok(Self::Move(Direction::Up)),
-                (KeyCode::Down, _) => Ok(Self::Move(Direction::Down)),
-                (KeyCode::Left, _) => Ok(Self::Move(Direction::Left)),
-                (KeyCode::Right, _) => Ok(Self::Move(Direction::Right)),
-                (KeyCode::PageDown, _) => Ok(Self::Move(Direction::PageDown)),
-                (KeyCode::PageUp, _) => Ok(Self::Move(Direction::PageUp)),
-                (KeyCode::Home, _) => Ok(Self::Move(Direction::Home)),
-                (KeyCode::End, _) => Ok(Self::Move(Direction::End)),
-                (KeyCode::Backspace, _) => Ok(Self::Backspace),
-                (KeyCode::Delete, _) => Ok(Self::Delete),
-                _ => Err(format!("Key Code not supported: {code:?}")),
-            },
-            Event::Resize(width_u16, height_u16) => Ok(Self::Resize(Size {
-                height: height_u16 as usize,
-                width: width_u16 as usize,
-            })),
-            _ => Err(format!("Event not supported: {event:?}"))
-        }
-    }
-}
+// #[allow(clippy::as_conversions)]
+// impl TryFrom<Event> for EditorCommand {
+//     type Error = String;
+// 
+//     fn try_from(event: Event) -> Result<Self, Self::Error> {
+//         match event {
+//             Event::Key(KeyEvent {
+//                 code, modifiers, ..
+//             }) => match (code, modifiers) {
+//                 (KeyCode::Char('q'), KeyModifiers::CONTROL) => Ok(Self::Quit),
+//                 (KeyCode::Char('s'), KeyModifiers::CONTROL) => Ok(Self::Save),
+//                 (KeyCode::Char(character), KeyModifiers::NONE | KeyModifiers::SHIFT) => {
+//                     Ok(Self::Insert(character))
+//                 },
+//                 (KeyCode::Tab, _) => Ok(Self::Insert('\t')),
+//                 (KeyCode::Enter, _) => Ok(Self::Enter),
+//                 (KeyCode::Up, _) => Ok(Self::Move(Direction::Up)),
+//                 (KeyCode::Down, _) => Ok(Self::Move(Direction::Down)),
+//                 (KeyCode::Left, _) => Ok(Self::Move(Direction::Left)),
+//                 (KeyCode::Right, _) => Ok(Self::Move(Direction::Right)),
+//                 (KeyCode::PageDown, _) => Ok(Self::Move(Direction::PageDown)),
+//                 (KeyCode::PageUp, _) => Ok(Self::Move(Direction::PageUp)),
+//                 (KeyCode::Home, _) => Ok(Self::Move(Direction::Home)),
+//                 (KeyCode::End, _) => Ok(Self::Move(Direction::End)),
+//                 (KeyCode::Backspace, _) => Ok(Self::Backspace),
+//                 (KeyCode::Delete, _) => Ok(Self::Delete),
+//                 _ => Err(format!("Key Code not supported: {code:?}")),
+//             },
+//             Event::Resize(width_u16, height_u16) => Ok(Self::Resize(Size {
+//                 height: height_u16 as usize,
+//                 width: width_u16 as usize,
+//             })),
+//             _ => Err(format!("Event not supported: {event:?}"))
+//         }
+//     }
+// }
